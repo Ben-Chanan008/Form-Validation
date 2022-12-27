f1e675cdfe11c24535081b8294211f56146c659a
BEN KEN IDEHEN <bchanan.boss@gmail.com>
2022-12-27 20:19:11 +0100

    this is my commit

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const nameInput = document.querySelector('#name');
 const myForm = document.querySelector('#myform');
 const phoneInput = document.querySelector('#number');
-const button= document.querySelector('#submit');
+const message = document.querySelector('.msg');
 
 myForm.addEventListener('submit', onSubmit);
 function onSubmit(e){
-    e.preventDefault();
+    // e.preventDefault();
+    console.log("success")
+    // if(nameInput.value === '' || phoneInput.value === ''){
+    //     message.classList.add('empty');
+    //     message.innerHTML = 'PLEASE ENTER FIELDS';
+    // }
 }
